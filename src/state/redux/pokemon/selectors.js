f74a11eb516647914017e4b58cd475b2eb34acde
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2020-01-11 22:47:51 +0700

    Create redux selectors

---
+++ src/state/redux/pokemon/selectors.js
@@ -0,0 +1,34 @@
+import { createSelector } from 'reselect';
+
+import getPokemonId from 'utils/getPokemonId';
+import { initialState } from './reducers';
+
+const pokemonSelector = state => state.pokemonReducers || initialState;
+const pokemonListSelector = state => state.pokemonReducers.pokemonList.data;
+
+const newPokemonlistSelector = createSelector(
+  pokemonListSelector,
+  pokemonList => pokemonList.map(pokemon => {
+    const id = getPokemonId(pokemon.url);
+    return {
+      ...pokemon,
+      id,
+      image: `${process.env.REACT_APP_IMAGE_URL}${id}.png`,
+    }
+  }),
+);
+
+const pokemonPageSelector = () => 
+  createSelector(
+    pokemonSelector,
+    newPokemonlistSelector,
+    (state, pokemonList) => ({
+      ...state,
+      pokemonList: {
+        ...state.pokemonList,
+        data: pokemonList,
+      },
+    }),
+  )
+
+export default pokemonPageSelector;
